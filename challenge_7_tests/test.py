4d8da0662560baab0a76b640306a44d1dc2cfdcf
Connor <connormullett@gmail.com>
2018-10-23 11:31:24 -0400

    initial

---
+++ challenge_7_tests/test.py
@@ -0,0 +1,13 @@
+
+import unittest
+
+class ChallengeSevenTests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_method(self):
+        pass
+
+    def tearDown(self):
+        pass
---
+++ challenge_8_tests/test.py
@@ -0,0 +1,13 @@
+
+import unittest
+
+class ChallengeEightTests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_method(self):
+        pass
+
+    def tearDown(self):
+        pass
